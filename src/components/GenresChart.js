e35c024fa66975c9b553a841c4c7cbb56a8c2af5
davidtom <davidjtomczyk@gmail.com>
2017-09-03 19:28:20 -0400

    update comments

--- src/components/GenresChart.js
+++ src/components/GenresChart.js
@@ -2,9 +2,10 @@ import React from 'react';
 import appendChart from './CreateGenreChart'
 
 // TODO:
+// BIG ONE: find a better way to display genres!! Bubble chart tutorial on Medium might be good
 // Clean up folders and file names - all, but especially this file and CreateGenreChart
 // Donut chart appending is super weird - fix this; ideally allowing clicking on genres button to re-render chart
-// Clean up JSON data from api: don't need total count, consider collapsing smaller genres
+// Clean up JSON data from api: don't need total count
 // Continue developing donut chart from tutorial
 // Stretch: add animation to donut chart creation
 
