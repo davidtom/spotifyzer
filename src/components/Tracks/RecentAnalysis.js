65fc4b54390262e2dc24569aa707fafc41093d6d
davidtom <davidjtomczyk@gmail.com>
2017-09-12 18:49:29 -0400

    prevent component from rendering unless there is data to show

--- src/components/Tracks/RecentAnalysis.js
+++ src/components/Tracks/RecentAnalysis.js
@@ -3,12 +3,14 @@ import {Segment} from 'semantic-ui-react'
 
 const RecentAnalysis = ({hours, minutes, perHour}) => {
 
+  // render segment only if there is data to put into it
   return (
+    (hours &&
     <Segment className="data-analysis-container" inverted padded compact textAlign="left">
       <h1>Analysis of Your Recent Track Play History</h1>
         <p>{hours} hours {Math.round(minutes)} minutes to play 50 tracks</p>
         <p>{(Math.round(perHour*100)/100)} tracks per hour</p>
-    </Segment>
+    </Segment>)
   );
 };
 
