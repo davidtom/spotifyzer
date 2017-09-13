24874a109d4d68d19fac2322d2930dfae5cca3ec
davidtom <davidjtomczyk@gmail.com>
2017-09-13 17:19:45 -0400

    update styling, background, and favicon

--- src/components/PageAssets/Footer.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { Header, Image, Loader, Segment } from 'semantic-ui-react'
-
-export const SiteFooter = () => (
-  <footer>Created by David Tomczyk during Flatiron School Web-060517</footer>
-)
