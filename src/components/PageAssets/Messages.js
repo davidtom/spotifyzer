8cbe2007cbb6b744c6913fbb7499e56177e93c74
davidtom <davidjtomczyk@gmail.com>
2017-09-13 12:13:06 -0400

    lock library updates to once per day

--- src/components/PageAssets/Messages.js
+++ src/components/PageAssets/Messages.js
@@ -29,6 +29,22 @@ export const PageToolTip = ({message}) => (
 )
 
 export const LastLibraryUpdate = ({date, updateLibrary}) => {
+
+  // Define function to get today's date formatted to match the date coming from
+  // backend
+  const dateToday = function(){
+    var today = new Date()
+    var dd = today.getDate()
+    var mm = today.getMonth()+1 //January is 0
+    var yyyy = today.getFullYear()
+    if(dd<10) {dd = '0'+dd}
+    if(mm<10) {mm = '0'+mm}
+
+    // return in format: "2017-09-13"
+    today = yyyy + '-' + mm + '-' + dd;
+    return today;
+  }
+
   return (
     <Segment className='library-update-message' compact inverted>
       <div>
@@ -36,10 +52,15 @@ export const LastLibraryUpdate = ({date, updateLibrary}) => {
         header='Your library data was last updated on:'
         content={date}/>
       </div>
-      <Button inverted animated size='large' onClick={updateLibrary}>
+      {/*Prevent users from updating more than once per day*/}
+      {dateToday() === date ?
+        <p>You may update your library again tomorrow!</p>
+        :
+        <Button inverted animated size='large' onClick={updateLibrary}>
         <Button.Content visible>Update</Button.Content>
         <Button.Content hidden><Icon inverted color='green' name='repeat' /></Button.Content>
-      </Button>
+        </Button>
+      }
     </Segment>
   )
 }
